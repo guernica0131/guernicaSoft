f9b09d912a95acb45dc4cdeabdf1487b1bb60a52
Adam <guernica0131@gmail.com>
2014-09-17 11:56:15 +0500

    Update to the portal controller

--- app/components/portals/portal.js
+++ app/components/portals/portal.js
@@ -1,43 +1,18 @@
- 
- angular.module('gSoft.portal', [])
-
-    // .config(['$routeProvider', function($routeProvider) {
-    //  	$routeProvider.when('/view1', {
-    //    	templateUrl: 'views/view1/view1.html',
-    //    	controller: 'View1Ctrl'
-    //  	});
-    // }])
+(function() {
+    angular.module('gSoft.portal', [])
 
     .directive('portals', function() {
         return {
             restrict: 'E',
             templateUrl: 'components/portals/portal.html',
-            controller: function($scope, $location, ImageLocation ) {
-            	//console.log("This is the portal element", ImageLocation.retrieve('THUMBS', 'global') );
-                
-
-
-                    
-
-
+            controller: function($scope, $location, ImageLocation) {
+                // this gives our portal template access to the ImageLocation object
                 $scope.images = ImageLocation.retrieve;
-                
-                // function() {
-                //     console.log("Getting pulled");
-                //     return {
-                //         pull : ImageLocation.retrieve
-                //     }
-                //     //return "get MEEERERE;";
-                // }'
-
-
-                //$scoImageLocation.retrieve()
-
-
 
             },
             controllerAs: 'Portal',
             scope: true
-          ///  location: true
         }
     });
+
+})();
